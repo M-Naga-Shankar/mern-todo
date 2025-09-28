08dfb1071a0c5b84e7c728ad33c769098feb6eb9
M-Naga-Shankar <logger3845@gmail.com>
2025-09-28 23:15:33 +0530

    env

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -27,3 +27,39 @@ export const loginUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateuser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: `Cannot find any user with ID ${id}` });             
+        }   
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: `Cannot find any user with ID ${id}` });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};  
+
